cedd1463d13fed812bf645bca0fa0444d867375c
Sergii <wheelyhog@gmail.com>
2023-04-06 10:58:25 +0200

    some changes

--- wheelyhog.js
+++ wheelyhog.js
@@ -1,7 +1,11 @@
 //Спроектируйте схему реляционной БД для проекта онлайн-курсов.
 
-users.id (1:M) course.lesson_id
-users.id (1:M) courses.teacher_id
-workers.id (1:M) users.manager_id
-orders.id (1:M) orders_details.order_id
-orders_details.product_id (M:1) products.id
+users.id (1:M) enrollments.user_id
+
+courses.id (1:M) enrollments.course_id
+
+courses.id (1:M) lessons.course_id
+
+lessons.id (1:M) progress.lesson_id
+
+users.id (1:M) progress.user_id
