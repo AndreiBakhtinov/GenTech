9405355acb690fe996b7e614b0432e26568966e1
Sergii <wheelyhog@gmail.com>
2023-04-06 14:01:39 +0200

    sql

--- wheelyhog.js
+++ wheelyhog.js
@@ -38,20 +38,45 @@ Table shippers {
   shipper_id integer
 }
 
-Ref: categories.category_id > products.product_id
+Table customers {
+  customer_id integer
+}
 
 
+Table categories {
+  category_id integer
+}
+
+Table employees {
+  employee_id integer
+}
+
+Table order_details {
+  order_detail_id integer
+}
+
+Table products {
+  product_id integer
+}
+
+Table orders {
+  order_id integer
+}
+
+Table shippers {
+  shipper_id integer
+}
+
+Ref: categories.category_id < products.product_id
 
 
+Ref: "orders"."order_id" > "customers"."customer_id"
 
 
-Ref: "orders"."order_id" < "customers"."customer_id"
-
-
-Ref: "customers"."customer_id" < "employees"."employee_id"
+Ref: "customers"."customer_id" > "employees"."employee_id"
 
 Ref: "order_details"."order_detail_id" < "products"."product_id"
 
-Ref: "orders"."order_id" < "employees"."employee_id"
+Ref: "orders"."order_id" > "employees"."employee_id"
 
 Ref: "shippers"."shipper_id" < "orders"."order_id"
